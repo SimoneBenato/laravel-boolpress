ef733c8980785d252239c2a15c2833b1496a4ed6
SimoneBenato <sb270298@gmail.com>
2021-11-10 15:36:36 +0100

    Creazione scaffolding e parte di autenticazione

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.backoffice')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+           <h1>Pagina di backoffice</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.frontoffice')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+           <h1>Pagina di frontoffice</h1>
+        </div>
+    </div>
+</div>
+@endsection
